ca2bf0780ef64a3e99cb228afc660710c6610c80
Baptiste Roseau <baptiste.roseau@tehtris.com>
2024-01-11 06:52:12 -0800

    nvim: Update plugins list and theme

--- home/.config/nvim/lua/baptiste/plugins.lua
+++ home/.config/nvim/lua/baptiste/plugins.lua
@@ -151,26 +151,18 @@ require('lazy').setup({
     },
 
     {
-        -- Theme inspired by Atom
-        'navarasu/onedark.nvim',
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme 'onedark'
-        end,
-    },
-
-    {
         -- Set lualine as statusline
         'nvim-lualine/lualine.nvim',
         -- See `:help lualine.txt`
         opts = {
             options = {
-                icons_enabled = false,
-                theme = 'onedark',
+                icons_enabled = true,
+                theme = 'horizon',
                 component_separators = '|',
                 section_separators = '',
             },
         },
+         dependencies = { 'nvim-tree/nvim-web-devicons' },
     },
 
     {
